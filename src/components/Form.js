db56a6be9da567d10686bbf7c9ca350dae694fc1
Nathan Vitor <nathanvitor38@gmail.com>
2022-07-15 10:58:34 -0300

    Requisito 2: Aprovado

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import ProtTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      // hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
+
     return (
       <form>
         <label htmlFor="name">
-          <input id="name" type="text" data-testid="name-input" />
+          <input
+            id="name"
+            type="text"
+            data-testid="name-input"
+            value={ cardName }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="description">
@@ -13,43 +35,91 @@ class Form extends React.Component {
             id="description"
             type="textarea"
             data-testid="description-input"
+            value={ cardDescription }
+            onChange={ onInputChange }
           />
         </label>
 
         <label htmlFor="attr1">
-          <input id="attr1" type="number" data-testid="attr1-input" />
+          <input
+            id="attr1"
+            type="number"
+            data-testid="attr1-input"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="attr2">
-          <input id="attr2" type="number" data-testid="attr2-input" />
+          <input
+            id="attr2"
+            type="number"
+            data-testid="attr2-input"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="attr3">
-          <input id="attr3" type="number" data-testid="attr3-input" />
+          <input
+            id="attr3"
+            type="number"
+            data-testid="attr3-input"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="image">
-          <input id="image" type="text" data-testid="image-input" />
+          <input
+            id="image"
+            type="text"
+            data-testid="image-input"
+            value={ cardImage }
+            onChange={ onInputChange }
+          />
         </label>
 
-        <div>
-          <select data-testid="rare-input">
-            <option>normal</option>
-            <option>raro</option>
-            <option>muito raro</option>
-          </select>
-        </div>
+        <select
+          data-testid="rare-input"
+          value={ cardRare }
+          onChange={ onInputChange }
+        >
+          <option>normal</option>
+          <option>raro</option>
+          <option>muito raro</option>
+        </select>
 
-        <label htmlFor="trunfo">
+        <label htmlFor="trunfo" checked={ cardTrunfo } onChange={ onInputChange }>
           <input id="trunfo" type="checkbox" data-testid="trunfo-input" />
         </label>
 
-        <button type="button" data-testid="save-button">
+        <button
+          type="button"
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onChange={ onSaveButtonClick }
+        >
           Salvar
         </button>
       </form>
     );
   }
 }
+
+Form.propTypes = {
+  cardName: ProtTypes.string.isRequired,
+  cardDescription: ProtTypes.string.isRequired,
+  cardAttr1: ProtTypes.string.isRequired,
+  cardAttr2: ProtTypes.string.isRequired,
+  cardAttr3: ProtTypes.string.isRequired,
+  cardImage: ProtTypes.string.isRequired,
+  cardRare: ProtTypes.string.isRequired,
+  cardTrunfo: ProtTypes.bool.isRequired,
+  // hasTrunfo: ProtTypes.bool.isRequired,
+  isSaveButtonDisabled: ProtTypes.bool.isRequired,
+  onInputChange: ProtTypes.func.isRequired,
+  onSaveButtonClick: ProtTypes.func.isRequired,
+};
 
 export default Form;
