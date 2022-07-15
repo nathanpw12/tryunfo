b7aca8c4b3333b1225f61ba6d92db281d97c0ef5
Nathan Vitor <nathanvitor38@gmail.com>
2022-07-15 10:33:24 -0300

    Requisito 1:  Eslint Fix

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,39 +1,52 @@
-import React from "react";
+import React from 'react';
 
 class Form extends React.Component {
   render() {
     return (
       <form>
-        <label>
-          <input type="text" data-testid="name-input" />
-        </label>
-        <label>
-          <input type="textarea" data-testid="description-input" />
-        </label>
-        <label>
-          <input type="number" data-testid="attr1-input" />
-        </label>
-        <label>
-          <input type="number" data-testid="attr2-input" />
-        </label>
-        <label>
-          <input type="number" data-testid="attr3-input" />
-        </label>
-        <label>
-          <input type="text" data-testid="image-input" />
+        <label htmlFor="name">
+          <input id="name" type="text" data-testid="name-input" />
         </label>
 
-        <select data-testid="rare-input">
-          <option>normal</option>
-          <option>raro</option>
-          <option>muito raro</option>
-        </select>
-
-        <label>
-          <input type="checkbox" data-testid="trunfo-input" />
+        <label htmlFor="description">
+          <input
+            id="description"
+            type="textarea"
+            data-testid="description-input"
+          />
         </label>
 
-        <button data-testid="save-button">Salvar</button>
+        <label htmlFor="attr1">
+          <input id="attr1" type="number" data-testid="attr1-input" />
+        </label>
+
+        <label htmlFor="attr2">
+          <input id="attr2" type="number" data-testid="attr2-input" />
+        </label>
+
+        <label htmlFor="attr3">
+          <input id="attr3" type="number" data-testid="attr3-input" />
+        </label>
+
+        <label htmlFor="image">
+          <input id="image" type="text" data-testid="image-input" />
+        </label>
+
+        <div>
+          <select data-testid="rare-input">
+            <option>normal</option>
+            <option>raro</option>
+            <option>muito raro</option>
+          </select>
+        </div>
+
+        <label htmlFor="trunfo">
+          <input id="trunfo" type="checkbox" data-testid="trunfo-input" />
+        </label>
+
+        <button type="button" data-testid="save-button">
+          Salvar
+        </button>
       </form>
     );
   }
